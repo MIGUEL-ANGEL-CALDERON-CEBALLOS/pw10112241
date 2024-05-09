ff73d45d832d386508e68113a23f78ca4b018e3f
MIGUEL-ANGEL-CALDERON-CEBALLOS <116331052+MIGUEL-ANGEL-CALDERON-CEBALLOS@users.noreply.github.com>
2024-05-09 10:57:39 -0700

    menu con clientes

---
+++ frontend/tienda/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import MenuView from './views/MenuView.vue'
+</script>
+
+<template>
+  <header>
+    <!-- la tipografia tiene derecho de autor -->
+    <!-- <h1>Hola</h1>  -->
+    <MenuView/>
+  </header>
+  
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
