35c4bd8672610de13ee2607115b78f17bc9c9c21
MIGUEL-ANGEL-CALDERON-CEBALLOS <116331052+MIGUEL-ANGEL-CALDERON-CEBALLOS@users.noreply.github.com>
2024-05-21 10:54:04 -0700

    agregar clientes con validaciones

--- frontend/tienda/src/views/ClientesCreateView.vue
+++ frontend/tienda/src/views/ClientesCreateView.vue
@@ -8,41 +8,51 @@
                 </div>
             </div>
             <div class="card-body">
+                <Form :validation-schema="validationSchema" @submit="onTodoBien">
                 <div class="mb-3">
                     Id
-                    <input type="text" class="form-control" v-model="model.cliente.id">
+                    <Field name="id" id="id" type="number" class="form-control" v-model="model.cliente.id"/>
+                    <ErrorMessage name="id" class="errorValidacion"/>
                 </div>
                 <div class="mb-3">
                     Nombre
-                    <input type="text" class="form-control" v-model="model.cliente.nombre">
+                    <Field name="nombre" id="nombre" type="text" class="form-control" v-model="model.cliente.nombre"/>
+                    <ErrorMessage name="nombre" class="errorValidacion"/>
                 </div>
                 <div class="mb-3">
                     Apellido
-                    <input type="text" class="form-control" v-model="model.cliente.apellido">
+                    <Field name="apellido" id="apellido" apellidotype="text" class="form-control" v-model="model.cliente.apellido"/>
+                    <ErrorMessage name="apellido" class="errorValidacion"/>
                 </div>
                 <div class="mb-3">
                     Direccion
-                    <input type="text" class="form-control" v-model="model.cliente.direccion">
+                    <Field name="direccion" id="direccion" type="text" class="form-control" v-model="model.cliente.direccion"/>
+                    <ErrorMessage name="direccion" class="errorValidacion"/>
                 </div>
                 <div class="mb-3">
                     Telefono
-                    <input type="text" class="form-control" v-model="model.cliente.telefono">
+                    <Field name="telefono" id="telefono" type="text" class="form-control" v-model="model.cliente.telefono"/>
+                    <ErrorMessage name="telefono" class="errorValidacion"/>
                 </div>
                 <div class="mb-3">
                     RFC
-                    <input type="text" class="form-control" v-model="model.cliente.rfc">
+                    <Field name="rfc" id="rfc" type="text" class="form-control" v-model="model.cliente.rfc"/>
+                    <ErrorMessage name="rfc" class="errorValidacion"/>
                 </div>
                 <div class="mb-3">
                     CURP
-                    <input type="text" class="form-control" v-model="model.cliente.curp">
+                    <Field name="curp" id="curp" type="text" class="form-control" v-model="model.cliente.curp"/>
+                    <ErrorMessage name="curp" class="errorValidacion"/>
                 </div>
                 <div class="mb-3">
                     CP
-                    <input type="text" class="form-control" v-model="model.cliente.cp">
+                    <Field name="cp" id="cp" type="text" class="form-control" v-model="model.cliente.cp"/>
+                    <ErrorMessage name="cp" class="errorValidacion"/>
                 </div>
                 <div class="mb-3">
-                    <button class="btn btn-primary" @click="guardarCliente()">Guardar</button>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
                 </div>
+            </Form>
             </div>
         </div>
     </div>
@@ -50,11 +60,42 @@
 
 <script>
 import axios from 'axios';
+import {Field, Form,ErrorMessage} from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as zod from 'zod';
 
 export default {
     name: 'ClientesCreate',
+    components: {Field, Form,ErrorMessage},
     data() {
+        const phoneRegex = new RegExp(
+            /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+        );
+        const rfcRegex = new RegExp(
+            /^([a-z]{3,4})(\d{2})(\d{2})(\d{2})([0-9a-z]{3})$/i
+        );
+        const curpRegex = new RegExp(
+            /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9][12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/g
+        );
+        const cpRegex = new RegExp(
+            /^[0-9]{5}$/
+            //para checar validaciones famosas es buscando 
+            //REGEX CURP MEXICO
+        );
+        const validationSchema = toTypedSchema(
+            zod.object({
+                id: zod.number({message: 'Solo numeros'}).int(),
+                nombre: zod.string().min(1,{message: 'Requerido'}),
+                apellido: zod.string().min(1,{message: 'Requerido'}),
+                direccion: zod.string().min(1,{message: 'Requerido'}),
+                telefono: zod.string().regex(phoneRegex, 'Numero no valido').min(10,{message:'Minimo 10'}),
+                rfc: zod.string().regex(rfcRegex, 'RFC no valido'),
+                curp: zod.string().regex(curpRegex, 'CURP no valida'),
+                cp: zod.string().regex(cpRegex,'Codigo postal no valido'),
+            })
+        );//lleva ; ?
         return {
+            validationSchema,
             mensaje: 0,
             model: {
                 cliente: {
@@ -71,6 +112,9 @@ export default {
         }
     },
     methods: {
+        onTodoBien(){
+            alert('Todo validado');
+        },
         guardarCliente() {
             axios.post('http://localhost:3000/api/clientes', this.model.cliente).then(res => {
                 if(res.data.affectedRows == 1){ //si insertamos 1 registro
@@ -91,5 +135,12 @@ export default {
             });
         }
     }
+    //siempre poner el estilo en la parte de style
 }
 </script>
+<style scoped> 
+    .errorValidacion{
+        color: red;
+        font-weight: bold;
+    }
+</style>
